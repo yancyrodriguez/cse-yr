5eaf2b3d9d9cb70bdc440aefb90a889991fcd7d8
yancyrodriguez <yancy.veraliz.rodriguez@gmail.com>
2024-09-21 10:23:00 -0600

    prep 4 assignment done

--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -1,9 +1,53 @@
 using System;
+using System.Collections.Generic;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep4 World!");
+        List<int> numbers = new List<int>();
+        int number;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished");
+        
+        do
+        {
+            Console.Write("Enter a number:");
+            number = int.Parse(Console.ReadLine());
+
+            if (number != 0)
+
+            {
+                numbers.Add(number);
+            }
+        } while (number != 0);
+
+        int sum = 0;
+        foreach (int num in numbers)
+
+        {
+            sum += num;
+        }
+
+        //Average
+
+        double average = (double)sum / numbers.Count;
+
+        //the largest number
+
+        int largestNumber = int.MinValue;
+        foreach (int num in numbers)
+        {
+            if (num > largestNumber)
+            {
+                largestNumber = num;
+            }
+
+        }
+
+        Console.WriteLine($"The sum is {sum}");
+        Console.WriteLine($"The average is {average}");
+        Console.WriteLine($"The largest number is {largestNumber}");
+
     }
 }

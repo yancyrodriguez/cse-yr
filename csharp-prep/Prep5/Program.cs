4cdee45c5a570a26ec5c1363cdeb265b8d57bc92
yancyrodriguez <yancy.veraliz.rodriguez@gmail.com>
2024-09-21 16:17:15 -0600

    Prep1 assignment completed

--- csharp-prep/Prep5/Program.cs
+++ csharp-prep/Prep5/Program.cs
@@ -4,6 +4,22 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep5 World!");
+    
+        Console.WriteLine("Welcome to the program!");
+
+        
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        
+        Console.Write("Please enter your favorite number: ");
+        int favoriteNumber = int.Parse(Console.ReadLine());
+
+        
+        int square = favoriteNumber * favoriteNumber;
+
+
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    
     }
 }
